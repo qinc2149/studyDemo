3efc7268bafdcc6bd872b5242a8320742410221b
qinc2149 <964235895@qq.com>
2017-12-31 10:53:55 +0800

    第3次提交

--- studyDemo/src/main/java/qinc/demo/com/App.java
+++ studyDemo/src/main/java/qinc/demo/com/App.java
@@ -1,7 +1,26 @@
 package qinc.demo.com;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
- * Created by ADMIN on 2017/12/31.
+ * @author qinc
+ * @version V1.0
+ * @Description:
+ * @Date 2017/9/26 17:16
  */
+@ComponentScan(basePackages = "qinc.demo.com")
+@EnableAutoConfiguration
+@EnableCaching //开启缓存注解
+@EnableScheduling//开启定时任务注解
+@EnableAsync//开启异步调用注解
 public class App {
+    public static  void main(String[] args){
+        SpringApplication.run(App.class, args);
+    }
 }
+
